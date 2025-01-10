dc278916e46f9b4214c6ee52797ba0be55ae5611
Kristiyan Yochev <kris.yochev@gmail.com>
2025-01-10 22:14:58 +0100

    Got open ai to extract json

---
+++ backend/ai_processing.py
@@ -0,0 +1,61 @@
+from openai import OpenAI
+from typing import Iterable
+from domain import ParsedDrawing
+import logging
+import re
+import json
+
+
+def connect_to_ai(api_key: str) -> OpenAI:
+    return OpenAI(api_key=api_key)
+
+
+def extract_json(text: str) -> dict | list:
+    JSON_REGEX = re.compile(r"```json\n(.*?)\n```", re.DOTALL)
+    matches = JSON_REGEX.search(text)
+    json_text = matches.group(1)
+    return json.loads(json_text)
+
+
+def parse_tables(images_png_base64: Iterable[str], client: OpenAI) -> dict:
+    format_description = None
+    with open("table_extraction_json_schema.txt") as f:
+        format_description = f.read()
+    if format_description is None:
+        raise ValueError("Could not read format description")
+
+    table_extraction_prompt = f"""
+You are an experienced architect and/or civil engineer. I need you to give me the data from the
+provided tables, which are extracted from technical drawings of prefabbed building elements. Please,
+give the data in a JSON format I will provide bellow. Use the data from every table. If an image
+does not look like a table, disregard it.
+
+```json
+{format_description}
+```
+"""
+    open_ai_images = [
+        {
+            "type": "image_url",
+            "image_url": {"url": f"data:image/png;base64,{image}", "detail": "high"},
+        }
+        for image in images_png_base64
+    ]
+    open_ai_response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": table_extraction_prompt},
+                    *open_ai_images,
+                ],
+            }
+        ],
+    )
+
+    response_text = open_ai_response.choices[0].message.content
+
+    print("OpenAI object", open_ai_response)
+
+    return extract_json(response_text)
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,43 @@
-from pdf_processing import load_document
+from pdf_processing import (
+    load_document,
+    extract_drawing_tables,
+    filter_tables,
+    get_table_pixmap,
+    pixmap_to_base64_png,
+)
+from ai_processing import parse_tables, connect_to_ai
 
 if __name__ == "__main__":
     import base64
+    import os
+    from dotenv import load_dotenv
+    import logging
+    from pprint import pprint
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.StreamHandler()],
+    )
+
+    load_dotenv()
+
     b64_data = None
-    with open("../drawings/Hackathon/01_Pfetten/FT_XX_01-104_a_F.pdf", "rb") as pdf_file:
+    with open(
+        "../drawings/Hackathon/01_Pfetten/FT_XX_01-104_a_F.pdf", "rb"
+    ) as pdf_file:
         content = pdf_file.read()
-        b64_data = base64.b64encode(content).decode('ascii')
-    
+        b64_data = base64.b64encode(content).decode("ascii")
+
     document = load_document(b64_data)
-    print(document[0])
+    logging.info("Loaded document")
+    page = document[0]
+    tables = filter_tables(extract_drawing_tables(page))
+    logging.info("Extracted tables")
+    pixmaps = (get_table_pixmap(page, table) for table in tables)
+    base64_images = (pixmap_to_base64_png(pixmap) for pixmap in pixmaps)
+
+    ai_client = connect_to_ai(os.getenv("OPEN_AI_KEY"))
+    ai_response = parse_tables(base64_images, ai_client)
+    print("OPENAI RESPONSE")
+    pprint(ai_response)
