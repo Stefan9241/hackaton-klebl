c991b4744c30966f482fd4ef9ca338254b0dd815
Kristiyan Yochev <kris.yochev@gmail.com>
2025-01-11 18:20:34 +0100

    Added webserver backend

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,21 @@ from pdf_processing import (
 # from ai_processing import parse_tables, connect_to_ai
 from gemini_processing import parse_tables, connect_to_ai
 
+
+def extract_pdf_data(pdf_base64: str) -> dict:
+    document = load_document(pdf_base64)
+    logging.info("Loaded document")
+    page = document[0]
+    tables = filter_tables(extract_drawing_tables(page))
+    logging.info("Extracted tables")
+    pixmaps = (get_table_pixmap(page, table) for table in tables)
+    base64_images = (pixmap_to_base64_png(pixmap) for pixmap in pixmaps)
+
+    ai_client = connect_to_ai(os.getenv("GEMINI_API_KEY"))
+    ai_response = parse_tables(base64_images, ai_client)
+    return ai_response
+
+
 if __name__ == "__main__":
     import base64
     import os
@@ -32,17 +47,6 @@ if __name__ == "__main__":
         content = pdf_file.read()
         b64_data = base64.b64encode(content).decode("ascii")
 
-    document = load_document(b64_data)
-    logging.info("Loaded document")
-    page = document[0]
-    tables = filter_tables(extract_drawing_tables(page))
-    logging.info("Extracted tables")
-    pixmaps = (get_table_pixmap(page, table) for table in tables)
-    # for i, pixmap in enumerate(pixmaps):
-    #     save_pixmap(pixmap, f"table-{i}.png")
-    base64_images = (pixmap_to_base64_png(pixmap) for pixmap in pixmaps)
-
-    ai_client = connect_to_ai(os.getenv("GEMINI_API_KEY"))
-    ai_response = parse_tables(base64_images, ai_client)
+    ai_response = extract_pdf_data(b64_data)
     print("GEMINI RESPONSE")
     pprint(ai_response)
